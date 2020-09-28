ee9183f6e46c827a7c12b8e72ed31a6f598092cb
onikeichut <nguyenquangduy91@gmail.com>
2020-09-28 10:56:27 +0700

    Tạo entities

---
+++ eSaleSolution.Data/EF/eSaleDBContext.cs
@@ -0,0 +1,18 @@
+﻿using eSaleSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eSaleSolution.Data.EF
+{
+    public class eSaleDBContext : DbContext
+    {
+        public eSaleDBContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<UDTItem> UDTItems { get; set; }
+
+    }
+}
---
+++ eSaleSolution.Data/Entities/UDTItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eSaleSolution.Data.Entities
+{
+    public class UDTItem
+    {
+        public string ref1 { set; get;}
+        public string ma_vt { set; get; }
+        public string dvt { set; get; }
+        public decimal sl_dh { set; get; }
+        public string gc_td1 { set; get; }
+        public string phan_loai { set; get; }
+        public string lead_time { set; get; }
+        public string s1 { set; get; }
+
+}
+}
